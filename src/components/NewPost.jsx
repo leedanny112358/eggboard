bbea90187484dc0a06100c27941aa0bce516d2d0
Daniel Inhee Lee <lee.danny112358@gmail.com>
2020-11-13 19:24:02 -0800

    file name issue

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -3,7 +3,11 @@ import React, { Component } from "react";
 class NewPost extends Component {
   state = {};
   render() {
-    return <div className="container">This is the newpost page</div>;
+    return (
+      <div className="container">
+        <h1>Create a New Posting</h1>
+      </div>
+    );
   }
 }
 
