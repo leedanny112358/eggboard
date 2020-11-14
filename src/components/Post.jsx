49df70afcf5ab2f8742545aa98253ac2f06694b1
Daniel Inhee Lee <lee.danny112358@gmail.com>
2020-11-13 19:27:00 -0800

    typo on jsx

---
+++ src/components/Post.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+/* IMPORTANT 
+PLEASE ONLY MODIFY THIS COMPONENT IN BRANCH POSTS
+*/
+
+class Post extends Component {
+  state = {};
+  render() {
+    return (
+      // PLEASE ADD CODE TO IMPLEMENT POSTS LAYOUT IN DIV POST //
+      <div className="container">
+        <h2>Hello World</h2>
+      </div>
+    );
+  }
+}
+
+export default Post;
