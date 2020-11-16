05734e96eaa4e2d26db15e86d56e1e245dad6655
jchao537 <jchao537@github.com>
2020-11-16 09:56:19 +0800

    implemented Post

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import NewPost from "./components/NewPost.jsx";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
+/* const testposts = [
+  { title: 'Post 1', manager: 'Jonathan Joestar', contact: 'firstjojo@gmail.com', description: 'I am making a cool project.', needs: 'I need hamon users', tags: ['Web App', 'MERN', 'Flexible'] },
+  { title: 'Post 2', manager: 'Joseph Joestar', contact: 'coolestjojo@gmail.com', description: 'I am making a cool project.', needs: 'I need hamon users', tags: ['Mobile App', 'React Native'] },
+  { title: 'Post 3', manager: 'Jotaro Kujo', contact: 'starplatinum@gmail.com', needs: 'Stand users only.', tags: ['Web App'] },
+  { title: 'Post 4', manager: 'Josuke Higashikata' },
+  { title: 'Post 5', manager: 'Giorno Giovanna' },
+  { title: 'Post 6', manager: 'Jolyne Kujo' },
+  { title: 'Post 7', manager: 'Johnny Joestar' },
+]; */
+
 function App() {
   return (
     <div className="App">
@@ -14,6 +24,7 @@ function App() {
             <NewPost />
           </Route>
           <Route path="/">
+            {/* <div className="PostContainer"> { testposts.map((post) => <Post title={post.title} manager={post.manager} contact={post.contact} description={post.description} needs={post.needs} tags={post.tags} />) } </div> */}
             <Post />
           </Route>
         </Switch>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,6 +5,7 @@ PLEASE ONLY MODIFY THIS COMPONENT IN BRANCH POSTS
 */
 
 class Post extends Component {
+  /*
   state = {};
   render() {
     return (
@@ -13,7 +14,46 @@ class Post extends Component {
         <h2>Hello World</h2>
       </div>
     );
-  }
+  }*/
+  constructor(props) {
+    super(props);
+    this.state = {
+        title: props.title || 'No Title',
+        manager: props.manager || 'No manager',
+        contact: props.contact || 'No contact info',
+        description: props.description || 'No description provided',
+        needs: props.needs || 'No needs provided',
+        tags: props.tags || ['tag1', 'tag2', 'tag3']
+    }
+}
+
+render() {
+    return (
+        <div className='post-grid-container'>
+            <div className='post-photo'>
+              <div className='egg'></div>
+            </div>
+            <div className='post-title'>
+                <h2>{this.state.title}</h2>
+            </div>
+            <div className='post-manager-contact'>
+                <p><span className="attribute">Manager:</span> {this.state.manager}</p>
+                <p><span className="attribute">Contact:</span> {this.state.contact}</p>
+            </div>
+            <div className='post-tags'>
+                {this.state.tags.map((tag) => <div>{tag}</div>)}
+            </div>
+            <div className='post-description'>
+                <p className="attribute">Description:</p>
+                <p>{this.state.description}</p>
+            </div>
+            <div className='post-needs'>
+                <p className="attribute">Needs:</p>
+                <p>{this.state.needs}</p>
+            </div>
+        </div>
+    )
+}
 }
 
 export default Post;
