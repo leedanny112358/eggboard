9413e41f4f7bb98fe9fcb52b719ce56f6a07c843
jchao537 <jchao537@github.com>
2020-11-25 13:20:54 +0800

    implemented NewPost

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,13 +1,93 @@
 import React, { Component } from "react";
 
 class NewPost extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="container">
-        <h1>Create a New Posting</h1>
+  constructor(props){ 
+    super(props) 
+    this.state = { title:'', name:'', contact:'', description:'', needs:''} 
+    this.handleChange = this.handleChange.bind(this) 
+    this.handleSubmit = this.handleSubmit.bind(this) 
+  } 
+
+  handleSubmit(event){ 
+    const { title, name, contact, description, needs } = this.state;
+    if (title === '' || name === '' || contact === '' || description === '' || needs === ''){
+      alert('Please enter all fields!');
+      return;
+    }
+    event.preventDefault();
+    alert(` 
+      ____New Post Details____\n 
+      Title : ${title} 
+      Name : ${name} 
+      Contact : ${contact} 
+      Description : ${description} 
+      Needs : ${needs} 
+    `);
+    return;
+  } 
+
+  handleChange(event){ 
+    this.setState({ 
+      [event.target.name] : event.target.value 
+    }) 
+  } 
+
+  render(){ 
+    return( 
+      <div className='centered-form'>
+        <form onSubmit={this.handleSubmit}> 
+          <div> 
+            <label>Project Title</label> 
+            <br/>
+            <input
+              name='title'
+              placeholder='Title' 
+              onChange={this.handleChange} 
+            /> 
+          </div>
+          <div> 
+            <label>Project Manager Name</label> 
+            <br/>
+            <input 
+              name='name' 
+              placeholder='Name'
+              onChange={this.handleChange} 
+            /> 
+          </div> 
+          <div> 
+            <label>Project Manager Contact</label> 
+            <br/>
+            <input 
+              name='contact' 
+              placeholder='Contact'
+              onChange={this.handleChange} 
+            /> 
+          </div> 
+          <div> 
+            <label>Project Description</label> 
+            <br/>
+            <input 
+              name='description' 
+              placeholder='Description'
+              onChange={this.handleChange} 
+            /> 
+          </div> 
+          <div> 
+            <label>Project Needs</label> 
+            <br/>
+            <input 
+              name='needs' 
+              placeholder='Needs'
+              value={this.state.phoneNo} 
+              onChange={this.handleChange} 
+            /> 
+          </div> 
+          <div> 
+            <button className='submit-button'>Submit Post</button> 
+          </div> 
+        </form> 
       </div>
-    );
+    ) 
   }
 }
 
