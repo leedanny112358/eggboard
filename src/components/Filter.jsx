d759687955e7ec3d7db3a92a8430aa65ebd3e8a8
Mercedes <mercedes.m.allende@gmail.com>
2020-11-29 22:11:33 -0800

    filter button colors and formatting

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -6,26 +6,27 @@ class Filter extends Component {
     return(
       <div id="filters"> 
         <div className="filter-grid-container">
-          <div className="filter-grid-item-lang">Java</div>
-          <div className="filter-grid-item-lang">Python</div>
-          <div className="filter-grid-item-lang">C/C++</div>
-          <div className="filter-grid-item-lang">HTML/CSS/JavaScript</div>
-          <div className="filter-grid-item-lang">SQL</div>
-          <div className="filter-grid-item-lang">Swift</div>
-          <div className="filter-grid-item-lang">Objective C</div>
-          <div className="filter-grid-item-lang">C#</div>
-          <div className="filter-grid-item-spec">Back-End</div>
-          <div className="filter-grid-item-spec">Front-End</div>
-          <div className="filter-grid-item-spec">Web Dev</div>
-          <div className="filter-grid-item-spec">Mobile/iOS</div>
-          <div className="filter-grid-item-spec">Low-Level</div>
-          <div className="filter-grid-item-spec">Software Tools</div>
-          <div className="filter-grid-item-spec">Security</div>
-          <div className="filter-grid-item-spec">Cloud Computing</div>
-          <div className="filter-grid-item-cat">Just for fun</div>
-          <div className="filter-grid-item-cat">For class</div>
-          <div className="filter-grid-item-cat">Start-up</div>
-          <div className="filter-grid-item-cat">Open source</div>
+          <button className="filter-grid-item-lang">Java</button>
+          <button className="filter-grid-item-lang">Python</button>
+          <button className="filter-grid-item-lang">C/C++</button>
+          <button className="filter-grid-item-lang">HTML/CSS/JavaScript</button>
+          <button className="filter-grid-item-lang">SQL</button>
+          <button className="filter-grid-item-lang">Swift</button>
+          <button className="filter-grid-item-lang">Objective C</button>
+          <button className="filter-grid-item-lang">C#</button>
+          <button className="filter-grid-item-spec">Back-End</button>
+          <button className="filter-grid-item-spec">Front-End</button>
+          <button className="filter-grid-item-spec">Web Dev</button>
+          <button className="filter-grid-item-spec">Mobile/iOS</button>
+          <button className="filter-grid-item-spec">Low-Level</button>
+          <button className="filter-grid-item-spec">Software Tools</button>
+          <button className="filter-grid-item-spec">Security</button>
+          <button className="filter-grid-item-spec">Cloud Computing</button>
+          <button className="filter-grid-item-cat">Just for fun</button>
+          <button className="filter-grid-item-cat">For class</button>
+          <button className="filter-grid-item-cat">Start-up</button>
+          <button className="filter-grid-item-cat">Open source</button>
+          <button className="filter-go"> Go </button>
         </div>
       </div>
       );
