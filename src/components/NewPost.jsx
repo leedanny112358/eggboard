58028abd15a6256e01800e1f69ef8faee544a7d9
Daniel Inhee Lee <lee.danny112358@gmail.com>
2020-11-29 17:31:20 -0800

    new post submit works

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,20 +1,45 @@
 import React, { Component } from "react";
 
 class NewPost extends Component {
-  constructor(props){ 
-    super(props) 
-    this.state = { title:'', name:'', contact:'', description:'', needs:''} 
-    this.handleChange = this.handleChange.bind(this) 
-    this.handleSubmit = this.handleSubmit.bind(this) 
-  } 
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      name: "",
+      contact: "",
+      description: "",
+      needs: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  handleSubmit(event){ 
+  async handleSubmit(event) {
     const { title, name, contact, description, needs } = this.state;
-    if (title === '' || name === '' || contact === '' || description === '' || needs === ''){
-      alert('Please enter all fields!');
+    if (
+      title === "" ||
+      name === "" ||
+      contact === "" ||
+      description === "" ||
+      needs === ""
+    ) {
+      alert("Please enter all fields!");
       return;
     }
     event.preventDefault();
+    try {
+      await fetch("http://localhost:5000/newpost", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.state),
+      });
+      console.log("request was sent!");
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    /* 
     alert(` 
       ____New Post Details____\n 
       Title : ${title} 
@@ -22,72 +47,72 @@ class NewPost extends Component {
       Contact : ${contact} 
       Description : ${description} 
       Needs : ${needs} 
-    `);
+    `); */
     return;
-  } 
+  }
 
-  handleChange(event){ 
-    this.setState({ 
-      [event.target.name] : event.target.value 
-    }) 
-  } 
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
 
-  render(){ 
-    return( 
-      <div className='centered-form'>
-        <form onSubmit={this.handleSubmit}> 
-          <div> 
-            <label>Project Title</label> 
-            <br/>
+  render() {
+    return (
+      <div className="centered-form">
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>Project Title</label>
+            <br />
             <input
-              name='title'
-              placeholder='Title' 
-              onChange={this.handleChange} 
-            /> 
+              name="title"
+              placeholder="Title"
+              onChange={this.handleChange}
+            />
           </div>
-          <div> 
-            <label>Project Manager Name</label> 
-            <br/>
-            <input 
-              name='name' 
-              placeholder='Name'
-              onChange={this.handleChange} 
-            /> 
-          </div> 
-          <div> 
-            <label>Project Manager Contact</label> 
-            <br/>
-            <input 
-              name='contact' 
-              placeholder='Contact'
-              onChange={this.handleChange} 
-            /> 
-          </div> 
-          <div> 
-            <label>Project Description</label> 
-            <br/>
-            <input 
-              name='description' 
-              placeholder='Description'
-              onChange={this.handleChange} 
-            /> 
-          </div> 
-          <div> 
-            <label>Project Needs</label> 
-            <br/>
-            <input 
-              name='needs' 
-              placeholder='Needs'
-              value={this.state.phoneNo} 
-              onChange={this.handleChange} 
-            /> 
-          </div> 
-          <div> 
-            <button className='submit-button'>Submit Post</button> 
-          </div> 
-        </form> 
+          <div>
+            <label>Project Manager Name</label>
+            <br />
+            <input
+              name="name"
+              placeholder="Name"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label>Project Manager Contact</label>
+            <br />
+            <input
+              name="contact"
+              placeholder="Contact"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label>Project Description</label>
+            <br />
+            <input
+              name="description"
+              placeholder="Description"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <label>Project Needs</label>
+            <br />
+            <input
+              name="needs"
+              placeholder="Needs"
+              value={this.state.phoneNo}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button className="submit-button">Submit Post</button>
+          </div>
+        </form>
       </div>
-    ) 
+    );
   }
 }
 
