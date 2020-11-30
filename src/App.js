cfd99b564fc71ea93bdac405285ee09ccad56611
Daniel Inhee Lee <lee.danny112358@gmail.com>
2020-11-29 19:01:23 -0800

    mapping works

--- src/App.js
+++ src/App.js
@@ -2,18 +2,9 @@ import Header from "./components/Header.jsx";
 import Post from "./components/Post.jsx";
 import NewPost from "./components/NewPost.jsx";
 import Filter from "./components/Filter.jsx";
+import Results from "./components/Results.jsx";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-
-/* const testposts = [
-  { title: 'Post 1', manager: 'Jonathan Joestar', contact: 'firstjojo@gmail.com', description: 'I am making a cool project.', needs: 'I need hamon users', tags: ['Web App', 'MERN', 'Flexible'] },
-  { title: 'Post 2', manager: 'Joseph Joestar', contact: 'coolestjojo@gmail.com', description: 'I am making a cool project.', needs: 'I need hamon users', tags: ['Mobile App', 'React Native'] },
-  { title: 'Post 3', manager: 'Jotaro Kujo', contact: 'starplatinum@gmail.com', needs: 'Stand users only.', tags: ['Web App'] },
-  { title: 'Post 4', manager: 'Josuke Higashikata' },
-  { title: 'Post 5', manager: 'Giorno Giovanna' },
-  { title: 'Post 6', manager: 'Jolyne Kujo' },
-  { title: 'Post 7', manager: 'Johnny Joestar' },
-]; */
 
 function App() {
   return (
@@ -25,7 +16,6 @@ function App() {
             <NewPost />
           </Route>
           <Route path="/">
-            {/* <div className="PostContainer"> { testposts.map((post) => <Post title={post.title} manager={post.manager} contact={post.contact} description={post.description} needs={post.needs} tags={post.tags} />) } </div> */}
             <button
               onClick={() => {
                 let x = document.getElementById("filters");
@@ -39,7 +29,7 @@ function App() {
               filters
             </button>
             <Filter />
-            <Post />
+            <Results />
           </Route>
         </Switch>
       </Router>
---
+++ src/components/Results.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Post from "./Post.jsx";
+
+class Results extends Component {
+  state = {
+    posts: [],
+  };
+  render() {
+    return (
+      <div className="PostContainer">
+        {this.state.posts.map((post) => (
+          <Post
+            title={post.title}
+            manager={post.manager}
+            contact={post.contact}
+            description={post.description}
+            needs={post.needs}
+            tags={post.tags}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/posts")
+      .then((response) => response.json())
+      .then((data) => this.setState({ posts: data }));
+  }
+}
+
+export default Results;
