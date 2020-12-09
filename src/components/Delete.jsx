126cf90b498889523ded49e9b2897def29c29fab
Daniel Inhee Lee <lee.danny112358@gmail.com>
2020-12-08 18:32:13 -0800

    delete fix

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,55 +1,52 @@
 import React, { Component } from "react";
 
 export default class Delete extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputPass: null,
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputPass: null,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleClick() {
-        this.props.visible();
-    }
+  handleClick = () => {
+    this.props.visible();
+  };
 
-    handleChange(event) {
-        this.setState({
-            inputPass: event.target.value,
-        })
-    }
+  handleChange(event) {
+    this.setState({
+      inputPass: event.target.value,
+    });
+  }
 
-    handleSubmit(event) {
+  handleSubmit(event) {}
 
-    }
-
-    render() {
-        return (
-            <div className="pop-up">
-                <div className="pop-up-content">
-                    <span className="pop-up-exit" onClick={this.handleClick}>
-                        &times;
-                    </span>
-                    <form>
-                        <h3>Delete selected post</h3>
-                        <label>
-                            Post-specific passcode:
-                            <input 
-                                type="password"
-                                id="passcode"
-                                name="passcode"
-                                placeholder="1234"
-                                value={this.state.inputPass}
-                                onChange={this.handleChange}
-                            />
-                        </label>
-                        <br />
-                        <input type="submit" />
-                    </form>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="pop-up">
+        <div className="pop-up-content">
+          <span className="pop-up-exit" onClick={this.handleClick}>
+            &times;
+          </span>
+          <form>
+            <h3>Delete selected post</h3>
+            <label>
+              Post-specific passcode:
+              <input
+                type="password"
+                id="passcode"
+                name="passcode"
+                placeholder="1234"
+                value={this.state.inputPass}
+                onChange={this.handleChange}
+              />
+            </label>
+            <br />
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
